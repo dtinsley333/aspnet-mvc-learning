0201465a014ea60ff8268b367da1af923e1bd42d
dtinsley333 <denise.tinsley@tekcongrp.com>
2016-03-30 11:09:02 -0500

    initial check in of hello world code example

---
+++ HelloWorld/HelloWorld/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HelloWorld.ViewModels;
+namespace HelloWorld.Controllers
+{
+    public class HomeController : Controller
+    {
+       
+        public ActionResult Index()
+        {
+
+            GreetingViewModel greetingViewModel = new GreetingViewModel
+            {
+                Greeting = "Good Day from Jim's Thai Restuarant! Thanks for being a regular customer.",
+                CurrentDate = DateTime.Now,
+                UserId = new Guid(),
+                UserFirstName="Bob",
+                UserLastName="Smith"
+
+            };
+            return View(greetingViewModel);
+            
+        }
+    }
+}
---
+++ HelloWorld/HelloWorld/ViewModels/GreetingViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HelloWorld.ViewModels
+{
+    public class GreetingViewModel
+    {//this is a comment to check my source code check in
+        public string Greeting { get; set; }
+        public DateTime CurrentDate { get; set; }
+        public Guid UserId { get; set; }
+        public string UserFirstName { get; set; }
+        public string UserLastName { get; set; }
+    }
+}
