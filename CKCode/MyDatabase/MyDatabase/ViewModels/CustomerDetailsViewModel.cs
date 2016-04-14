738bfbdbd2961f320146e68fb35f4d9bcb397963
dtinsley333 <denise.tinsley@tekcongrp.com>
2016-04-14 14:59:03 -0500

    Added a folder fixed Databases

---
+++ CKCode/MyDatabase/MyDatabase/ViewModels/CustomerDetailsViewModel.cs
@@ -0,0 +1,17 @@
+﻿using MyDatabase.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyDatabase.ViewModels
+{
+    public class CustomerDetailsViewModel
+    {
+        public int CustomerId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string ContactNumber { get; set; }
+        public List<Customer> Customers { get; set; }
+    }
+}
