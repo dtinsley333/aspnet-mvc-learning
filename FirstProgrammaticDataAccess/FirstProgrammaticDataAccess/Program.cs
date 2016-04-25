10d89f0ced5e9ad4257b5f97a9632cb3b7064f6b
dtinsley333 <denise.tinsley@tekcongrp.com>
2016-04-25 09:53:08 -0500

    Created 1st programmatic data access example.

--- FirstProgrammaticDataAccess/FirstProgrammaticDataAccess/Program.cs
+++ FirstProgrammaticDataAccess/FirstProgrammaticDataAccess/Program.cs
@@ -9,16 +9,63 @@ namespace FirstProgrammaticDataAccess
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append("select ani.CommonName,");
+            sb.Append("ani.Gender,ani.Species,");
+            sb.Append("ani.AcquiredDate,");
+            sb.Append("h.Name, h.[Description] ");
+            sb.Append("from Animals ani join Habitat h ");
+            sb.Append("on ani.HabitatId = h.HabitatId ");
+            sb.Append("order by ani.CommonName");
+            var query = sb.ToString();
+
+            // 1. Instantiate the connection
+            SqlConnection conn = new SqlConnection(
+                "Data Source=alienware-pc\\sqlexpress;Initial Catalog=Zoolandia;Integrated Security=True;Connect Timeout=15;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+
+            SqlDataReader rdr = null;
+
+            try
+            {
+                // 2. Open the connection
+                conn.Open();
+
+                // 3. Pass the connection to a command object
+                SqlCommand cmd = new SqlCommand(query, conn);
+
+                //
+                // 4. Use the connection
+                //
+
+                // get query results
+                rdr = cmd.ExecuteReader();
+
+                // print the CustomerID of each record
+                while (rdr.Read())
+                {
+                    Console.WriteLine("Animal Name: " + rdr[0]+ "  Habitat: " + rdr[4]);
+                }
+            }
+            finally
+            {
+                // close the reader
+                if (rdr != null)
+                {
+                    rdr.Close();
+                }
+
+                // 5. Close the connection
+                if (conn != null)
+                {
+                    conn.Close();
+                }
+                Console.ReadLine();
+
+            }
         }
-
-        private SqlConnection Connection;
-        private SqlCommand Command;
-        private SqlDataReader Reader;
-        private string results;
-
-        //Create a Connection object.
-        SqlConnection Connecton= New SqlConnection("Initial Catalog=Northwind;Data Source=localhost;Integrated Security=SSPI;');
     }
 }
+  
